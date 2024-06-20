05012a2a4b8bec627b447e2d20542f2884a826da
Colin Huber <cbhuber@gmail.com>
2024-06-19 18:44:36 -0600

    feat: get_merkle function

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -109,4 +109,28 @@ impl Chain {
         self.chain.push(block);
         true
     }
+
+    fn get_merkle(curr_trans: Vec<Transaction>) -> String {
+        let mut merkle = Vec::new();
+
+        for t in &curr_trans {
+            let hash = Chain::hash(t);
+
+            merkle.push(hash)
+        }
+
+        if merkle.len() % 2 == 1 {
+            let last = merkle.last().cloned().unwrap();
+            merkle.push(last);
+        }
+
+        while merkle.len() > 1 {
+            let mut h1 = merkle.remove(0);
+            let mut h2 = merkle.remove(0);
+            h1.push_str(&mut h2);
+            let nh = Chain::hash(&h1);
+            merkle.push(nh);
+        }
+        merkle.pop().unwrap()
+    }
 }
